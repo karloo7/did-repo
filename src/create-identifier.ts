909afd57b85662d710dbac75137138970cf12ffc
karloo7 <karlo.karacic@fri.uni-lj.si>
2024-09-06 08:48:15 +0200

    Option to select provider when creating DID.

--- src/create-identifier.ts
+++ src/create-identifier.ts
@@ -2,7 +2,9 @@ import { agent } from './veramo/setup.js'
 
 async function main() {
   const didAlias = process.argv[2] ?? 'default'
-  const identifier = await agent.didManagerCreate({ alias: didAlias })
+  const didProvider = process.argv[3]
+  console.log("\nselected provider " + didProvider + ".\n")
+  const identifier = await agent.didManagerCreate({ alias: didAlias, provider: didProvider })
   console.log(`New identifier created`)
   console.log(JSON.stringify(identifier, null, 2))
 }
